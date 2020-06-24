b1745158ea746e9c275ad8669141fa9f750bc683
Teije <teijevhemert@gmail.com>
2020-06-24 12:07:28 +0200

    Reworked structure to comply with naming scheme

---
+++ src/main/java/nl/inholland/tentamen/teijevanhemert/TeijevanhemertApplication.java
@@ -0,0 +1,13 @@
+package nl.inholland.tentamen.teijevanhemert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeijevanhemertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeijevanhemertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/inholland/tentamen/teijevanhemert/TeijevanhemertApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.inholland.tentamen.teijevanhemert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeijevanhemertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
