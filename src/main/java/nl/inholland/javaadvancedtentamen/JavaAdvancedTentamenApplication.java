7b86c779760ba8065502370966c3e8afb55a2cb3
Teije <teijevhemert@gmail.com>
2020-06-24 11:44:15 +0200

    Init

---
+++ src/main/java/nl/inholland/javaadvancedtentamen/JavaAdvancedTentamenApplication.java
@@ -0,0 +1,13 @@
+package nl.inholland.javaadvancedtentamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAdvancedTentamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAdvancedTentamenApplication.class, args);
+	}
+
+}
